9d8fad8e029655bc2419e0c7712762e61b02cb73
ssbzeref <nascentsurya@gmail.com>
2020-10-23 10:34:46 +0545

    initial setup for react app only

---
+++ react/src/user/pages/Users.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import UsersList from '../components/UserList';
+
+
+const Users = () => {
+    const USERS = [
+        {
+            id: "u1",
+            image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            name: "Shyam Sundar Bhattarai",
+            places: 5            
+        }
+    ];
+    return (
+        <UsersList items={USERS}/>
+    );
+}
+
+export default Users;
