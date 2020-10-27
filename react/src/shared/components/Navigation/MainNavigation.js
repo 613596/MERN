dbbced7665757dd285bea594df9fc235dc275bbf
ssbzeref <nascentsurya@gmail.com>
2020-10-27 15:11:49 +0545

    header and sidebar is on progress

---
+++ react/src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+import MainHeader from './MainHeader';
+import Navlinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+
+import './MainNavigation.css';
+
+const MainNavigation = (props) => {
+    return (
+        <React.Fragment>
+        <SideDrawer>
+            <nav className="main-navigation__drawer-nav">
+                <Navlinks />
+            </nav>
+        </SideDrawer>
+       <MainHeader>
+            <button className="main-navigation__menu-btn">
+                 <span />
+                 <span />
+                 <span />
+                 <span />
+            </button>
+            <h1 className="main-navigation__title">
+               <Link to="/"> Your Places </Link>
+            </h1>
+            <nav className="main-navigation__header-nav">
+                <Navlinks />
+            </nav>
+       </MainHeader>
+       </React.Fragment>
+    )
+}
+
+export default MainNavigation
